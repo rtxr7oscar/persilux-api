b74a8478d71d71f2fa2ddfe7f7512b8516c5a051
Oscar <oscarrtxr7@gmail.com>
2026-06-16 15:18:21 -0600

    fix cors headers

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Conexión a la Base de Datos MySQL
